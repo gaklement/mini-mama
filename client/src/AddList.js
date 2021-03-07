821358d3c9ff36ef1c1076a12ddcc0ef1f8455af
Gisa Klement <gisaklement@hotmail.com>
2021-03-07 18:32:51 +0100

    clears value on create

--- client/src/AddList.js
+++ client/src/AddList.js
@@ -1,15 +1,27 @@
 import React, { useState } from 'react'
 
 function AddList({ onCreateList }) {
-  const [name, setName] = useState()
+  const [name, setName] = useState('')
   return (
     <div>
       <input
         type="text"
         onChange={(event) => setName(event.target.value)}
-        onKeyDown={({ key }) => key === 'Enter' && onCreateList(name)}
+        onKeyDown={({ key }) => {
+          if (key === 'Enter') {
+            onCreateList(name)
+            setName('')
+          }
+        }}
+        value={name}
       />
-      <button disabled={!name} onClick={() => onCreateList(name)}>
+      <button
+        disabled={!name}
+        onClick={() => {
+          onCreateList(name)
+          setName('')
+        }}
+      >
         Add new list
       </button>
     </div>
