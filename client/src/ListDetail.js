163dd8920fa0039e10be4c3b9efa3df25886e1d8
Gisa Klement <gisaklement@hotmail.com>
2021-03-08 19:37:00 +0100

    adds input to list detail

--- client/src/ListDetail.js
+++ client/src/ListDetail.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function ListDetail({ history, match }) {
+  const [listItem, setListItem] = useState('')
+
   return (
     <div>
+      <button onClick={() => history.push('/')}>Zurück</button>
+      <div>ListDetail for {match.params.listId}</div>
+      <input
+        type="text"
+        value={listItem}
+        onChange={(event) => setListItem(event.target.value)}
+        onKeyDown={({ key }) => {
+          if (key === 'Enter') {
+            console.log('nein', listItem)
+            setListItem('')
+          }
+        }}
+      />
       <button
         onClick={() => {
-          history.push('/')
+          console.log('ja', listItem)
+          setListItem('')
         }}
       >
-        Zurück
+        Add item to list
       </button>
-      <div>ListDetail for {match.params.listId}</div>
     </div>
   )
 }
