c2bc0c8a3c7019478bea99323525df0e867e18e6
Gisa Klement <gisaklement@hotmail.com>
2021-06-12 23:46:50 +0200

    moves lists controller to mongodb

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,17 +1,59 @@
 const Datastore = require('nedb')
+const { MongoClient } = require('mongodb')
 
 const database = new Datastore('./database.db')
 database.loadDatabase()
 
+const user = 'Gisa'
+const password = 'IReallyLikeFood'
+
+const uri = `mongodb+srv://${user}:${password}@thenextcluster.gjiq0.mongodb.net/i-like-food?retryWrites=true&w=majority`
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+
 const list = (req, res, next) => {
-  // get a list
-  if (req.method === 'GET') {
-    database.find({ id: req.query.listId }, (_, list) => {
-      res.status(200).json({
-        ...list[0],
-      })
-    })
+  async function run() {
+    try {
+      await client.connect()
+      const mongoDatabase = client.db()
+
+      // use list here instead
+      const lists = mongoDatabase.collection('lists')
+
+      const query = { name: 'A database list' }
+      const databaseList = await lists.findOne(query)
+      console.log(databaseList)
+
+      //
+      // noch nciht gemacht
+      // get a list
+      if (req.method === 'GET') {
+        console.log('== helllo i run')
+        database.find({ id: req.query.listId }, (_, list) => {
+          res.status(200).json({
+            ...list[0],
+          })
+        })
+      }
+      //
+    } finally {
+      console.log('==but i also close')
+      // await client.close()
+    }
   }
+
+  run().catch(console.dir)
+
+  // // get a list
+  // if (req.method === 'GET') {
+  //   database.find({ id: req.query.listId }, (_, list) => {
+  //     res.status(200).json({
+  //       ...list[0],
+  //     })
+  //   })
+  // }
 
   // update list name
   if (req.method === 'PUT') {
@@ -36,53 +78,128 @@ const list = (req, res, next) => {
 }
 
 const lists = (req, res, next) => {
-  // get all lists
-  if (req.method === 'GET') {
-    database.find({}, (_, lists) => {
-      const sortedLists = lists.sort(
-        (listA, listB) =>
-          new Date(listA.createTime) - new Date(listB.createTime)
-      )
+  async function run() {
+    try {
+      await client.connect()
+      const mongoDatabase = client.db()
 
-      res.status(200).json({
-        lists: sortedLists,
-      })
-    })
+      // add new list
+      if (req.method === 'POST') {
+        const listId = Math.floor(100000 + Math.random() * 900000).toString()
+
+        await mongoDatabase.collection('lists').insertOne({
+          id: listId,
+          createTime: Date.now(),
+          name: req.body.name,
+          items: [],
+        })
+
+        res.status(200).json({
+          listId,
+        })
+      }
+
+      // get all lists
+      if (req.method === 'GET') {
+        const allLists = await mongoDatabase
+          .collection('lists')
+          .find({})
+          .toArray()
+
+        const sortedLists = allLists.sort(
+          (listA, listB) =>
+            new Date(listA.createTime) - new Date(listB.createTime)
+        )
+
+        res.status(200).json({
+          lists: sortedLists,
+        })
+      }
+
+      // add item to list
+      if (req.method === 'PUT') {
+        console.log('==req body id', req.body.id)
+        mongoDatabase.collection('lists').updateOne(
+          { id: req.body.id },
+          {
+            $push: {
+              items: {
+                id: Math.floor(100000 + Math.random() * 900000).toString(),
+                name: req.body.item,
+              },
+            },
+          }
+        )
+
+        // database.update(
+        //   { id: req.body.id },
+        //   {
+        //     $push: {
+        //       items: {
+        //         id: Math.floor(100000 + Math.random() * 900000).toString(),
+        //         name: req.body.item,
+        //       },
+        //     },
+        //   },
+        //   {}
+        // )
+
+        res.status(200).json({})
+      }
+    } finally {
+      // await client.close()
+    }
   }
+
+  run().catch(console.dir)
+
+  // // get all lists
+  // if (req.method === 'GET') {
+  //   database.find({}, (_, lists) => {
+  //     const sortedLists = lists.sort(
+  //       (listA, listB) =>
+  //         new Date(listA.createTime) - new Date(listB.createTime)
+  //     )
+
+  //     res.status(200).json({
+  //       lists: sortedLists,
+  //     })
+  //   })
+  // }
 
   // add new list
-  if (req.method === 'POST') {
-    const listId = Math.floor(100000 + Math.random() * 900000).toString()
+  // if (req.method === 'POST') {
+  //   const listId = Math.floor(100000 + Math.random() * 900000).toString()
 
-    database.insert({
-      id: listId,
-      createTime: Date.now(),
-      name: req.body.name,
-      items: [],
-    })
+  //   database.insert({
+  //     id: listId,
+  //     createTime: Date.now(),
+  //     name: req.body.name,
+  //     items: [],
+  //   })
 
-    res.status(200).json({
-      listId,
-    })
-  }
+  //   res.status(200).json({
+  //     listId,
+  //   })
+  // }
 
-  // add item to list
-  if (req.method === 'PUT') {
-    database.update(
-      { id: req.body.id },
-      {
-        $push: {
-          items: {
-            id: Math.floor(100000 + Math.random() * 900000).toString(),
-            name: req.body.item,
-          },
-        },
-      },
-      {}
-    )
+  // // add item to list
+  // if (req.method === 'PUT') {
+  //   database.update(
+  //     { id: req.body.id },
+  //     {
+  //       $push: {
+  //         items: {
+  //           id: Math.floor(100000 + Math.random() * 900000).toString(),
+  //           name: req.body.item,
+  //         },
+  //       },
+  //     },
+  //     {}
+  //   )
 
-    res.status(200).json({})
-  }
+  //   res.status(200).json({})
+  // }
 }
 
 const item = (req, res, next) => {
@@ -116,3 +233,11 @@ const item = (req, res, next) => {
 module.exports.lists = lists
 module.exports.list = list
 module.exports.item = item
+
+// move controllers into the async
+// rewrite controllers to use mongodb
+// keep the database connection open
+// maybe put all controller types (lists, list, item) into one giant module or maybe not
+// close db connection
+// id => _id
+// clean up
