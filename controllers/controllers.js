e0fc0d1ae40accec5bd128b5823e8d46ce35c434
Gisa Klement <gisaklement@hotmail.com>
2021-06-13 00:15:11 +0200

    moves deleting a list to mongodb

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -46,20 +46,27 @@ const list = (req, res, next) => {
 
         res.status(200).json({})
       }
+
+      // delete a list
+      if (req.method === 'DELETE') {
+        await mongoDatabase
+          .collection('lists')
+          .deleteOne({ id: req.query.listId })
+        res.status(200).json({})
+      }
     } finally {
-      console.log('==but i also close')
-      // await client.close()
+      await client.close()
     }
   }
 
   run().catch(console.dir)
 
-  // delete a list
-  if (req.method === 'DELETE') {
-    database.remove({ id: req.query.listId }, {}, () => {
-      res.status(200).json({})
-    })
-  }
+  // // delete a list
+  // if (req.method === 'DELETE') {
+  //   database.remove({ id: req.query.listId }, {}, () => {
+  //     res.status(200).json({})
+  //   })
+  // }
 }
 
 const lists = (req, res, next) => {
@@ -122,7 +129,7 @@ const lists = (req, res, next) => {
         res.status(200).json({})
       }
     } finally {
-      // await client.close()
+      await client.close()
     }
   }
 
@@ -146,8 +153,6 @@ const item = (req, res, next) => {
           .find({ id: req.body.listId })
           .toArray()
 
-        console.log('3==sadf', list)
-
         // find item in the list's items
         // update it and overwrite all items
         const updatedItems = list[0].items.map((item) =>
@@ -170,45 +175,20 @@ const item = (req, res, next) => {
         res.status(200).json({})
       }
     } finally {
+      await client.close()
     }
   }
 
   run().catch(console.dir)
-  // // toggle item from done/undone
-  // if (req.method === 'PUT') {
-  //   database.find({ id: req.body.listId }, (_, list) => {
-  //     // find item in the list's items
-  //     // update it and overwrite all items
-  //     const updatedItems = list[0].items.map((item) =>
-  //       item.id === req.body.itemId
-  //         ? {
-  //             ...item,
-  //             doneTime: item.done ? null : Date.now(),
-  //             done: !item.done,
-  //           }
-  //         : item
-  //     )
-
-  //     database.update(
-  //       { id: req.body.listId },
-  //       {
-  //         $set: { items: updatedItems },
-  //       }
-  //     )
-  //   })
-
-  //   res.status(200).json({})
-  // }
 }
 
 module.exports.lists = lists
 module.exports.list = list
 module.exports.item = item
 
-// move controllers into the async
-// rewrite controllers to use mongodb
 // keep the database connection open
 // maybe put all controller types (lists, list, item) into one giant module or maybe not
 // close db connection
 // id => _id
 // clean up
+// add loading messages in the client
